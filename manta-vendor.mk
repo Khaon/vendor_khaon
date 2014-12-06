92e9c64e3d91d30577a6435c86bbabd39839451a
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2014-12-06 11:58:45 +0100

    manta-vendor added

---
+++ manta-vendor.mk
@@ -0,0 +1,10 @@
+# Khaon's extra files
+
+#Hosts file and adway
+PRODUCT_COPY_FILES += \
+    vendor/khaon_misc/adaway/hosts:system/etc/hosts \
+    vendor/khaon_misc/adaway/org.adaway_51.apk:system/priv-app/org.adaway_51/org.adaway_51.apk
+
+#BBS
+PRODUCT_COPY_FILES += \
+   vendor/khaon_misc/BBS/BetterBatteryStats_xdaedition_2.0.0.0RC2.apk:system/priv-app/BetterBatteryStats_xdaedition_2.0.0.0RC2/BetterBatteryStats_xdaedition_2.0.0.0RC2.apk
