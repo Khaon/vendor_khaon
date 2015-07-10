7044a9a382705978ad633505df20bb19355b7969
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2015-07-10 22:53:49 +0200

    updated proprietary files

--- manta-vendor.mk
+++ manta-vendor.mk
@@ -2,20 +2,18 @@
 
 #Hosts file and adway
 PRODUCT_COPY_FILES += \
-    vendor/khaon_misc/adaway/hosts:system/etc/hosts \
-    vendor/khaon_misc/adaway/org.adaway_51.apk:system/priv-app/org.adaway_51/org.adaway_51.apk
+    vendor/khaon_misc/adaway/hosts:system/etc/hosts
+
+PRODUCT_PACKAGES += \
+    AdAway
 
 #BBS
-PRODUCT_COPY_FILES += \
-   vendor/khaon_misc/BBS/BetterBatteryStats_xdaedition_2.0.0.0.apk:system/priv-app/BetterBatteryStats_xdaedition_2.0.0.0/BetterBatteryStats_xdaedition_2.0.0.0.apk
-
-#Add Apollo 
 PRODUCT_PACKAGES += \
-    Apollo
+    BetterBatteryStats
     
-#Add v4a library
-PRODUCT_COPY_FILES += \
-	vendor/khaon_misc/viper4android/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so
+#Add v4a
+PRODUCT_PACKAGES += \
+	Viper4Android
 
 # Chromium Prebuilt
 ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
